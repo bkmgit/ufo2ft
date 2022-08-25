363a187bbea7e1eabbb714933783e9ed40e3bac4
Simon Cozens <simon@simon-cozens.org>
2022-08-25 17:46:30 +0100

    Revert "Only consider the default source for groups".
    Some fonts really are messy.
    
    This reverts commit 628f678743a2ed77228370c7ad7e2b2a16a1c7ea.

--- Lib/ufo2ft/featureWriters/variableKernWriter.py
+++ Lib/ufo2ft/featureWriters/variableKernWriter.py
@@ -22,27 +22,36 @@ class VariableKernFeatureWriter(KernFeatureWriter):
     def getKerningGroups(
         designspace: DesignSpaceDocument, glyphSet: dict[str, Any] | None = None
     ) -> tuple[dict[str, list[str]], dict[str, list[str]]]:
-        default_source = designspace.findDefault()
-        assert default_source is not None and default_source.font is not None
         if glyphSet:
             allGlyphs = set(glyphSet.keys())
         else:
+            default_source = designspace.findDefault()
+            assert default_source is not None and default_source.font is not None
             allGlyphs = set(default_source.font.keys())
         side1Groups: dict[str, list[str]] = {}
         side2Groups: dict[str, list[str]] = {}
-        # only consider the groups from the default source, as we do for
-        # instance generation.
-        for name, members in default_source.font.groups.items():
-            # prune non-existent or skipped glyphs and sort for determinism.
-            members = sorted(g for g in members if g in allGlyphs)
-            # skip empty groups
-            if not members:
-                continue
-            # skip groups without UFO3 public.kern{1,2} prefix
-            if name.startswith(SIDE1_PREFIX):
-                side1Groups[name] = members
-            elif name.startswith(SIDE2_PREFIX):
-                side2Groups[name] = members
+        for source in designspace.sources:
+            font = source.font
+            assert font is not None
+            for name, members in font.groups.items():
+                # prune non-existent or skipped glyphs
+                members = {g for g in members if g in allGlyphs}
+                # skip empty groups
+                if not members:
+                    continue
+                # skip groups without UFO3 public.kern{1,2} prefix
+                if name.startswith(SIDE1_PREFIX):
+                    group = side1Groups.get(name)
+                    if group is None:
+                        side1Groups[name] = sorted(members)
+                    elif set(group) != members:
+                        log_redefined_group("left", name, group, font, members)
+                elif name.startswith(SIDE2_PREFIX):
+                    group = side2Groups.get(name)
+                    if group is None:
+                        side2Groups[name] = sorted(members)
+                    elif set(group) != members:
+                        log_redefined_group("right", name, group, font, members)
         return side1Groups, side2Groups
 
     @staticmethod
@@ -97,3 +106,16 @@ class VariableKernFeatureWriter(KernFeatureWriter):
                     side2 = side2Classes[side2]
                 result.append(KerningPair(side1, side2, value))
         return result
+
+
+def log_redefined_group(
+    side: str, name: str, group: list[str], font: Any, members: set[str]
+) -> None:
+    log.warning(
+        "incompatible %s groups: %s was previously %s, %s tried to make it %s",
+        side,
+        name,
+        sorted(group),
+        font,
+        sorted(members),
+    )
